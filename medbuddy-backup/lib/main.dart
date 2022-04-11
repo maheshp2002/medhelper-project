ec941230e508d4f9a28d42335bbc4127d0c76845
unknown <anuragrajanp@gmail.com>
2022-04-11 19:35:13 +0530

    Update main.dart

--- medbuddy-backup/lib/main.dart
+++ medbuddy-backup/lib/main.dart
@@ -11,14 +11,14 @@ import 'package:provider/provider.dart';
 
 //for notification
 const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_chgannel', //id
-  'High Importance Notification', //title
-  'This channel is used for important notification', //description
-  importance: Importance.high,
-  playSound: true
-  );
+    'high_importance_channel', //id
+    'High Importance Notification', //title
+    'This channel is used for important notification', //description
+    importance: Importance.high,
+    playSound: true);
 //initiallization of flutter notification
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 //initialize firebase app during notification
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -26,20 +26,21 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print('A bg message just showed up : ${message.messageId}');
 }
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   await flutterLocalNotificationsPlugin
-  .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-  ?.createNotificationChannel(channel);
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
 
   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
     alert: true,
     badge: true,
     sound: true,
-    );
+  );
 
   runApp(MyApp());
 }
@@ -53,64 +54,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Splash(),
-      
     );
   }
-
 }
+
 class _medbuddy extends State<medbuddy> {
   GlobalBloc globalBloc;
 
   void initState() {
     globalBloc = GlobalBloc();
     super.initState();
-    FirebaseMessaging.onMessage.listen((RemoteMessage message){
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       RemoteNotification notification = message.notification;
       AndroidNotification android = message.notification?.android;
-      if(notification != null && android != null){
+      if (notification != null && android != null) {
         flutterLocalNotificationsPlugin.show(
-          notification.hashCode,
-          notification.title,
-          notification.body,
-          NotificationDetails(
-            android: AndroidNotificationDetails(
-              channel.id,
-              channel.name,
-              channel.description,
-              color: Colors.blue,
-              playSound: true,
-              //icon: '@minmap/ic_launcher',
-            ),
-          )
-        );
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                color: Colors.blue,
+                playSound: true,
+                //icon: '@minmap/ic_launcher',
+              ),
+            ));
       }
-
     });
 
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message){
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print('Time to take your medicine as per prescription');
       RemoteNotification notification = message.notification;
-      AndroidNotification android = message.notification?.android; 
-      if(notification != null && android != null){
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
         showDialog(
-          context: context,
-           builder: (_){
-             return AlertDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
                 title: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(notification.body)
-                    ],
-                  )),
-             );
-           }
-           );
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text(notification.body)],
+                )),
+              );
+            });
       }
     });
   }
@@ -145,10 +140,9 @@ void showNotification(){
           brightness: Brightness.light,
         ),
         home: //WelcomeScreen(),
-        login1(),
-       debugShowCheckedModeBanner: false,
+            login1(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
 }
-
